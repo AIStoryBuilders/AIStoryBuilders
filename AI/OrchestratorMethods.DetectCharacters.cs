1a12cf75b8f7c194ba3bbfcf177bc6f3fcd57e18
Michael Washington <webmaster@adefwebserver.com>
2023-11-25 08:26:14 -0800

    Added "DetectCharacters" method

---
+++ AI/OrchestratorMethods.DetectCharacters.cs
@@ -0,0 +1,122 @@
+﻿using AIStoryBuilders.Model;
+using AIStoryBuilders.Models.JSON;
+using OpenAI.Chat;
+using OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIStoryBuilders.Models;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AIStoryBuilders.AI
+{
+    public partial class OrchestratorMethods
+    {
+        #region public async Task<List<SimpleCharacterSelector>> DetectCharacters(Paragraph objParagraph)
+        public async Task<List<SimpleCharacterSelector>> DetectCharacters(Paragraph objParagraph)
+        {
+            string Organization = SettingsService.Organization;
+            string ApiKey = SettingsService.ApiKey;
+            string SystemMessage = "";
+            string GPTModel = "gpt-4-1106-preview";
+
+            ChatMessages = new List<ChatMessage>();
+
+            if (SettingsService.FastMode == true)
+            {
+                GPTModel = "gpt-3.5-turbo-1106";
+            }
+
+            LogService.WriteToLog($"Detect Characters using {GPTModel} - Start");
+
+            // Create a new OpenAIClient object
+            // with the provided API key and organization
+            var api = new OpenAIClient(new OpenAIAuthentication(ApiKey, Organization), null, new HttpClient() { Timeout = TimeSpan.FromSeconds(520) });
+
+            // Create a colection of chatPrompts
+            ChatResponse ChatResponseResult = new ChatResponse();
+            List<Message> chatPrompts = new List<Message>();
+
+            // *****************************************************
+            dynamic Databasefile = AIStoryBuildersDatabaseObject;
+
+            // Update System Message
+            SystemMessage = CreateDetectCharacters(objParagraph.ParagraphContent);
+
+            LogService.WriteToLog($"Prompt: {SystemMessage}");
+
+            chatPrompts = new List<Message>();
+
+            chatPrompts.Insert(0,
+            new Message(
+                Role.System,
+                SystemMessage
+                )
+            );
+
+            // Get a response from ChatGPT 
+            var FinalChatRequest = new ChatRequest(
+                chatPrompts,
+                model: GPTModel,
+                temperature: 0.0,
+                topP: 1,
+                frequencyPenalty: 0,
+                presencePenalty: 0,
+                responseFormat: ChatResponseFormat.Json);
+
+
+            ChatResponseResult = await api.ChatEndpoint.GetCompletionAsync(FinalChatRequest);
+
+            // *****************************************************
+
+            LogService.WriteToLog($"TotalTokens: {ChatResponseResult.Usage.TotalTokens} - ChatResponseResult - {ChatResponseResult.FirstChoice.Message.Content}");
+
+            List<SimpleCharacterSelector> colCharacterOutput = new List<SimpleCharacterSelector>();
+
+            try
+            {
+                // Convert the JSON to a list of SimpleCharacters
+
+                var JSONResult = ChatResponseResult.FirstChoice.Message.Content.ToString();
+
+                dynamic data = JObject.Parse(JSONResult);
+
+                foreach (var character in data.characters)
+                {
+                    string CharacterName = character.name.ToString();
+                    colCharacterOutput.Add(new SimpleCharacterSelector { CharacterDisplay = $"{CharacterName}", CharacterValue = $"{CharacterName}" });
+                }
+            }
+            catch (Exception ex)
+            {
+                LogService.WriteToLog($"Error - DetectCharacters: {ex.Message} {ex.StackTrace ?? ""}");
+            }
+
+            return colCharacterOutput;
+        }
+        #endregion
+
+        // Methods
+
+        #region private string CreateDetectCharacters(string paramParagraphContent)
+        private string CreateDetectCharacters(string paramParagraphContent)
+        {
+            return "You are a function that will produce only JSON. \n" +
+            "Please analyze a paragraph of text (given as #paramParagraphContent). \n" +
+            "#1 Identify all characters, by name, mentioned in the paragraph. \n" +
+            $"### This is the content of #paramParagraphContent: {paramParagraphContent} \n" +
+            "Provide the results in the following JSON format: \n" +
+            "{\n" +
+            "\"characters\": [\n" +
+            "{ \n" +
+            "\"name\": \"[Name]\" \n" +
+            "} \n" +
+            "] \n" +
+            "}";
+        }
+        #endregion
+    }
+}
