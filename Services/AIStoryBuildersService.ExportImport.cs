d4d5fb78a81e00e13cf9a4530ae47c792dacc506
Michael Washington <webmaster@adefwebserver.com>
2023-12-07 06:36:34 -0800

    Export to Word working

--- Services/AIStoryBuildersService.ExportImport.cs
+++ Services/AIStoryBuildersService.ExportImport.cs
@@ -2,14 +2,15 @@
 using AIStoryBuilders.Models.JSON;
 using Newtonsoft.Json;
 using System.Text.Json;
+using Xceed.Document.NET;
 using Xceed.Words.NET;
 
 namespace AIStoryBuilders.Services
 {
     public partial class AIStoryBuildersService
     {
-        #region public async Task ExportWordDocument(Story objStory)
-        public async Task<byte[]> ExportWordDocument(Story objStory)
+        #region public byte[] ExportWordDocument(Story objStory)
+        public byte[] ExportWordDocument(Story objStory)
         {
             try
             {
@@ -27,11 +28,33 @@ namespace AIStoryBuilders.Services
 
                 using (DocX document = DocX.Create(WordFilePath))
                 {
-                    // Add a new Paragraph to the document.
-                    var p = document.InsertParagraph();
+                    // Create Cover Page
+                    document.InsertParagraph(objStory.Title).FontSize(25d).SpacingAfter(50d).Alignment = Alignment.center;
+ 
+                    var colChapters = GetChapters(objStory);
 
-                    // Append some text.
-                    p.Append("Hello World").Font("Arial Black");
+                    foreach (var objChapter in colChapters)
+                    {
+                        document.InsertSectionPageBreak();
+
+                        // Create Chapter Title
+                        document.InsertParagraph(objChapter.ChapterName).FontSize(20d).SpacingAfter(25d).Alignment = Alignment.center;
+
+                        var colParagraphs = GetParagraphs(objChapter);
+
+                        foreach (var objParagraph in colParagraphs)
+                        {
+                            // Break up objParagraph.ParagraphContent by \n
+                            string[] sections = objParagraph.ParagraphContent.Split('\n');
+
+                            // Create a new paragraph for each line
+                            foreach (string section in sections)
+                            {
+                                var p = document.InsertParagraph(section);
+                                p.IndentationFirstLine = 28f;
+                            }
+                        }
+                    }
 
                     // Save the document.
                     document.Save();
