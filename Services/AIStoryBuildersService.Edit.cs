2127515bc0a9e9ea4f5a5e76ca000293f0730b0d
Michael Washington <webmaster@adefwebserver.com>
2023-12-08 19:02:36 -0800

    Adding RestructureParagraphs

---
+++ Services/AIStoryBuildersService.Edit.cs
@@ -0,0 +1,40 @@
+﻿using AIStoryBuilders.Models;
+using AIStoryBuilders.Models.JSON;
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace AIStoryBuilders.Services
+{
+    public partial class AIStoryBuildersService
+    {
+        #region public void RestructureParagraphs(Chapter objChapter, int ParagraphToRemove, string RestructureType)
+        public void RestructureParagraphs(Chapter objChapter, int ParagraphToRemove, string RestructureType)
+        {
+            try
+            {
+                
+            }
+            catch (Exception ex)
+            {
+                // Log error
+                LogService.WriteToLog("RestructureParagraphs: " + ex.Message + " " + ex.StackTrace ?? "" + " " + ex.InnerException.StackTrace ?? "");
+            }
+        }
+        #endregion
+
+        #region public void RestructureChapters(int ChapterNumber, string RestructureType)
+        public void RestructureChapters(int ChapterNumber, string RestructureType)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                // Log error
+                LogService.WriteToLog("RestructureChapters: " + ex.Message + " " + ex.StackTrace ?? "" + " " + ex.InnerException.StackTrace ?? "");
+            }
+        }
+        #endregion
+    }
+}
